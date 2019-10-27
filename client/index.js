36ceabd1989593a109fc445a567c8e5b1e0be3e0
yershalom <shalom.yerushalmy@forter.com>
2019-10-27 17:01:37 +0200

    first commit

---
+++ client/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.scss';
+const App = () => {
+  return <div>Welcome to React!</div>;
+};
+ReactDOM.render(<App />, document.getElementById('app'));
